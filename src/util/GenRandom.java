11185d979bcd6ee16cbbd9eb9347a89e61615fce
SA JAEHEON <jls15900@gmail.com>
2026-01-18 21:32:30 +0900

    refactor: 랜덤 값을 생성하는 메서드를 모아 놓는 유틸 클래스 작성

---
+++ src/util/GenRandom.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.util.Random;
+
+public class GenRandom {
+	
+	public static final Random random = new Random();
+	
+	private GenRandom() {}
+	
+    public static String generateRandomCardNumber() {
+        return String.format("%04d-****-****-%04d", 
+                random.nextInt(9000) + 1000, 
+                random.nextInt(9000) + 1000);
+    }
+
+    public static String getRandomMerchant() {
+        String[] merchants = {"스타벅스", "GS25", "쿠팡", "배달의민족", "카카오택시", "FISA구내식당"};
+        return merchants[random.nextInt(merchants.length)];
+    }
+
+    public static String getRandomErrorCode() {
+        String[] errors = {"잔액부족", "한도초과", "도난분실카드", "통신시간초과", "CVC오류"};
+        return errors[random.nextInt(errors.length)];
+    }
+}
